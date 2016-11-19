a986e15937bc6831a6854348f6a5fc40a22f22e3
lwo <lwo@iisg.nl>
2016-11-19 11:08:10 +0100

    Initial commit

---
+++ lineair.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+#
+# lineair.py --file [csv]
+#
+# CSV opbouw: datum, extra_aflossing, tarief, correctie
+
+import calendar
+import csv
+import datetime
+import decimal
+import getopt
+import sys
+
+
+def calculate(f, _schuld, _looptijd):
+    jaar = 0
+    afgelost = 0
+    schuld = _schuld
+    tarief = 0
+    maand = 0
+    looptijd = _looptijd
+    aflossing = get_decimal(schuld / looptijd)
+
+    print(
+    'extra_aflossing,jaar,datum,maand,maanden_over,tarief,rente,aflossing,afgelost,schuld_begin_maand,schuld,lasten')
+
+    with open(f, 'rb') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        next(reader, None)  # skip the headers
+        for items in reader:
+            _datum = get_date(items[0])
+            _extra_aflossing = items[1]
+            _tarief = get_decimal(items[2])
+            _anomaly = get_decimal(items[3])
+            datum = _datum
+            maand += 1
+            maanden_over = 1 + looptijd - maand
+            if datum.month == 1 or maand == 1:
+                jaar += 1
+                _jaar = str(jaar)
+            else:
+                _jaar = ''
+
+            if _tarief:
+                tarief = _tarief
+
+            schuld_begin_maand = schuld
+            afgelost += aflossing
+            schuld -= aflossing
+            rente = get_decimal((schuld_begin_maand / 1200) * tarief)
+            lasten = get_decimal(rente + aflossing)
+            if _extra_aflossing == '-1':
+                _extra_aflossing = int(schuld)
+            print(
+                '{},{},{:%d-%m-%Y},{},{},{}%,{:.2f},{:.2f},{:.2f},{:.2f},{:.2f},{:.2f}'.format(_extra_aflossing,
+                                                                                               _jaar, datum, maand,
+                                                                                               maanden_over,
+                                                                                               tarief,
+                                                                                               rente,
+                                                                                               aflossing, afgelost,
+                                                                                               schuld_begin_maand,
+                                                                                               schuld, lasten))
+
+            if _extra_aflossing:
+                extra_aflossing = get_decimal(_extra_aflossing)
+                afgelost += extra_aflossing
+                monthrange = calendar.monthrange(datum.year, datum.month)[1]
+                voor = get_decimal(((float(datum.day)) / monthrange) * rente)
+                rente_correctie = get_decimal(((schuld_begin_maand - extra_aflossing) / 1200) * tarief)
+                na = get_decimal(((float(monthrange - datum.day)) / monthrange) * rente_correctie)
+                correctie = rente - voor - na
+                schuld -= (get_decimal(extra_aflossing + correctie + _anomaly))
+                aflossing = get_decimal(schuld / (maanden_over - 1))
+
+            if schuld < 1:
+                sys.exit(0)
+
+def get_int(s):
+    if s:
+        return int(s)
+    else:
+        return 0
+
+
+def get_date(s):
+    return datetime.datetime.strptime(s, '%Y-%m-%d')
+
+
+def get_decimal(s):
+    if s:
+        return round(decimal.Decimal(s), 2)
+    else:
+        return decimal.Decimal(0)
+
+
+def add_months(start_date, months=1):
+    month = start_date.month - 1 + months
+    year = int(start_date.year + month / 12)
+    month = month % 12 + 1
+    day = min(start_date.day, calendar.monthrange(year, month)[1])
+    return datetime.datetime(year, month, day)
+
+
+def usage():
+    print('Usage: lineair.py --file [file to csv] --schuld [loan] --looptijd [months]')
+
+
+def main(argv):
+    f = looptijd = schuld = None
+
+    try:
+        opts, args = getopt.getopt(argv, 'f:l:s:h', ['file=', 'schuld=', 'looptijd=', 'help'])
+    except getopt.GetoptError as e:
+        print("Opt error: " + e.msg)
+        usage()
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ('-h', '--help'):
+            usage()
+            sys.exit()
+        elif opt in ('-f', '--file'):
+            f = arg
+        elif opt in ('-s', '--schuld'):
+            schuld = get_decimal(arg)
+        elif opt in ('-l', '--looptijd'):
+            looptijd = int(arg)
+        else:
+            print("Unknown argument: " + opt)
+            sys.exit(1)
+
+    assert f
+    assert looptijd
+    assert schuld
+    calculate(f, schuld, looptijd)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
